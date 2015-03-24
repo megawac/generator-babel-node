26174ae7a4d40fd1882113b9a65de878cee23703
Graeme Yeates <yeatesgraeme@gmail.com>
2015-03-24 09:32:29 -0400

    Attempt to fix user name guess bug

--- app/index.js
+++ app/index.js
@@ -12,6 +12,7 @@ var path = require('path');
 module.exports = yeoman.generators.Base.extend({
   initializing: function() {
     this.pkg = require('../package.json');
+    this.config = gitConfig.sync();
   },
 
   prompting: function() {
@@ -23,40 +24,44 @@ module.exports = yeoman.generators.Base.extend({
     ));
 
     npm.load(function() {
-      var config = gitConfig.sync();
-      var prompts = [{
-        type: 'input',
-        name: 'user',
-        message: 'What is your github username/organization?',
-        default: npm.whoami()
-      }, {
-        type: 'input',
-        name: 'repo',
-        message: 'What is your repo/projects name?',
-        default: kebabcase(this.appname)
-      }, {
-        type: 'input',
-        name: 'description',
-        message: 'What is a description of this project? (eg. A nice module.)'
-      }, {
-        type: 'input',
-        name: 'author',
-        message: 'Who is the author of this project?',
-        default: config.user.name + ' <' + config.user.email + '>'
-      }, {
-        type: 'input',
-        name: 'tags',
-        message: 'Are there any tags for your package (e.g. cure-for-world-hunger)'
-      }];
+      this.username = npm.whoami();
+      this._showPrompts(done);
+    }.bind(this));
+  },
 
-      this.prompt(prompts, function(props) {
-        this.user = props.user;
-        this.repo = props.repo;
-        this.description = props.description;
-        this.author = props.author;
-        this.tags = props.tags && props.tags.split(',');
-        done();
-      }.bind(this));
+  _showPrompts: function(done) {
+    var prompts = [{
+      type: 'input',
+      name: 'user',
+      message: 'What is your github username/organization?',
+      default: this.username
+    }, {
+      type: 'input',
+      name: 'repo',
+      message: 'What is your repo/projects name?',
+      default: kebabcase(this.appname)
+    }, {
+      type: 'input',
+      name: 'description',
+      message: 'What is a description of this project? (eg. A nice module.)'
+    }, {
+      type: 'input',
+      name: 'author',
+      message: 'Who is the author of this project?',
+      default: this.config.user.name + ' <' + this.config.user.email + '>'
+    }, {
+      type: 'input',
+      name: 'tags',
+      message: 'Are there any tags for your package (e.g. cure-for-world-hunger)'
+    }];
+
+    this.prompt(prompts, function(props) {
+      this.user = props.user;
+      this.repo = props.repo;
+      this.description = props.description;
+      this.author = props.author;
+      this.tags = props.tags && props.tags.split(',');
+      done();
     }.bind(this));
   },
 
